d336600c759b4fe5310f5030bf1b9c4840ad94dd
Kattyaalexa <kattyaalexa@hotmail.com>
2020-10-26 21:18:18 -0500

    Pagos, Busqueda y pasarela

---
+++ application/ms-Busqueda/src/test/java/com/puj/aes/producto/producto/MsBusquedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puj.aes.producto.producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBusquedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
