a5c73e5646384688c343193653e4b1a596c6f14a
Kattyaalexa <kattyaalexa@hotmail.com>
2020-10-26 22:23:50 -0500

    Eureka configuration

--- application/ms-pasarela/src/main/java/com/puj/aes/pasarela/pasarela/PasarelaApplication.java
+++ application/ms-pasarela/src/main/java/com/puj/aes/pasarela/pasarela/PasarelaApplication.java
@@ -1,9 +1,12 @@
-package com.puj.aes.producto.producto;
+package com.puj.aes.pasarela.pasarela;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
+
 public class PasarelaApplication {
 
 	public static void main(String[] args) {
