f2fa8267a288abe3b21c5cffed8ffb5c0c9d0d77
Kattyaalexa <kattyaalexa@hotmail.com>
2020-10-26 22:23:50 -0500

    Eureka configuration

--- application/ms-Busqueda/src/main/java/com/puj/aes/busqueda/busqueda/MsBusquedaApplication.java
+++ application/ms-Busqueda/src/main/java/com/puj/aes/busqueda/busqueda/MsBusquedaApplication.java
@@ -1,8 +1,10 @@
-package com.puj.aes.producto.producto;
+package com.puj.aes.busqueda.busqueda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MsBusquedaApplication {
 
