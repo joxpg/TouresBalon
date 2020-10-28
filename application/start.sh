385246b29c79cb94545ab852b42540dd0923cc68
Johann Fernando Trigos <joxpg@hotmail.com>
2020-10-27 22:05:49 -0500

    Se agrega docker-compose

--- application/start.sh
+++ application/start.sh
@@ -1,54 +1,47 @@
+docker-compose down -v
 cd eureka-server
 mvn install
 docker rm -f eureka-server
 docker build -t eureka-server:lastest .
-docker run -p 9040:9040 -d --name eureka-server eureka-server:lastest
 cd ..
 cd zuul
 mvn install
 docker rm -f zuul
 docker build -t zuul:lastest .
-docker run -p 9041:9041 -d --name zuul zuul:lastest
 cd ..
 cd calificacion
 mvn install
 docker rm -f calificacion
 docker build -t calificacion:lastest .
-docker run -p 9060:9060 -d --name calificacion calificacion:lastest
 cd ..
 cd reserva
 mvn install
 docker rm -f reserva
 docker build -t reserva:lastest .
-docker run -p 9061:9061 -d --name reserva reserva:lastest
 cd ..
 cd proveedor
 mvn install
 docker rm -f proveedor
 docker build -t proveedor:lastest .
-docker run -p 9062:9062 -d --name proveedor proveedor:lastest
 cd ..
 cd ms-Busqueda
 mvn install
-docker rm -f ms-Busqueda
-docker build -t ms-Busqueda:lastest .
-docker run -p 9070:9070 -d --name ms-Busqueda ms-Busqueda:lastest
+docker rm -f ms-busqueda
+docker build -t ms-busqueda:lastest .
 cd ..
 cd ms-Pagos
 mvn install
-docker rm -f ms-Pagos
-docker build -t ms-Pagos:lastest .
-docker run -p 9071:9071 -d --name ms-Pagos ms-Pagos:lastest
+docker rm -f ms-pagos
+docker build -t ms-pagos:lastest .
 cd ..
 cd ms-pasarela
 mvn install
 docker rm -f ms-pasarela
 docker build -t ms-pasarela:lastest .
-docker run -p 9072:9072 -d --name ms-pasarela ms-pasarela:lastest
 cd ..
 cd Producto
 mvn install
-docker rm -f Producto
-docker build -t Producto:lastest .
-docker run -p 9080:9080 -d --name Producto Producto:lastest
+docker rm -f producto
+docker build -t producto:lastest .
 cd ..
+docker-compose up -d
