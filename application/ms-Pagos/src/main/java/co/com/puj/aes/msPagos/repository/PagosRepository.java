7a8ed0df48b2066a1982834d27cf60fecf7381fc
Kattyaalexa <kattyaalexa@hotmail.com>
2020-10-27 23:18:33 -0500

    Correccion compilacion

--- application/ms-Pagos/src/main/java/co/com/puj/aes/msPagos/repository/PagosRepository.java
+++ application/ms-Pagos/src/main/java/co/com/puj/aes/msPagos/repository/PagosRepository.java
@@ -3,10 +3,12 @@ package co.com.puj.aes.msPagos.repository;
 import co.com.puj.aes.msPagos.entity.Pagos;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
 
+@Repository
 public interface PagosRepository extends CrudRepository<Pagos, Short>{
     Pagos findEpsByIdEps(Short idEps);
     Pagos findByIdEpsAndEstadoEpsTrue(Short idEps);
