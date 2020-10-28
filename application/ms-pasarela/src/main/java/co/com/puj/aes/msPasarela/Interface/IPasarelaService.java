0e83e1397cd83eff8d02a412da1ed0cc5ed09e3f
Kattyaalexa <kattyaalexa@hotmail.com>
2020-10-28 00:34:10 -0500

    Delete parela

--- application/ms-pasarela/src/main/java/co/com/puj/aes/msPasarela/Interface/IPasarelaService.java
+++
@@ -1,7 +0,0 @@
-package co.com.puj.aes.msPasarela.Interface;
-
-import co.com.puj.aes.msPasarela.Entity.Pasarela;
-
-public interface IPasarelaService {
-    public Pasarela enviarRespuesta(Pasarela respuesta);
-}
